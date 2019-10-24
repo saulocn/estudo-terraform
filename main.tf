4d45b91464713a3ab057bd393c1c146704d91279
Saulo de Carvalho Neto <sacarvalho@uolinc.com>
2019-10-23 23:43:16 -0300

    Adição de Database, uma nova região e uma máquina acessando ao db

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
+  version = "~> 2.0"
   region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "dev" {
@@ -29,6 +36,14 @@ resource "aws_instance" "dev4" {
   depends_on             = [aws_s3_bucket.dev4]
 }
 
+resource "aws_s3_bucket" "dev4" {
+  bucket = "saulocn-labs-dev4"
+  acl    = "private"
+
+  tags = {
+    Name = "saulocn-labs-dev4"
+  }
+}
 
 resource "aws_instance" "dev5" {
   ami           = "ami-026c8acd92718196b"
@@ -40,11 +55,33 @@ resource "aws_instance" "dev5" {
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
-resource "aws_s3_bucket" "dev4" {
-  bucket = "saulocn-labs-dev4"
-  acl    = "private"
 
+resource "aws_instance" "dev6" {
+  provider = "aws.us-east-2"
+  ami           = "ami-0d8f6eb4f641ef691"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
   tags = {
-    Name = "saulocn-labs-dev4"
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+  depends_on = ["aws_dynamodb_table.dynamodb-homologacao"]
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider       = "aws.us-east-2"
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
   }
 }
--- security-group.tf
+++ security-group.tf
@@ -14,3 +14,21 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = "aws.us-east-2"
+  name        = "acesso-ssh"
+  description = "Permissão de acesso via ssh"
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["200.221.157.57/32"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
